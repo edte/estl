855c51162c9dfb906107b7e9300b6482f8dda10e
张彩嵘 <940481948@qq.com>
2021-12-30 17:29:02 +0800

    feat(iterator): add iterator implements

---
+++ containers/vector/iterator.go
@@ -0,0 +1,144 @@
+// @program:     tiny-stl
+// @file:        iterator.go
+// @author:      edte
+// @create:      2021-12-30 12:47
+// @description:
+package vector
+
+import (
+	"stl/iterator"
+)
+
+type Iterator struct {
+	data *Vector
+	pos  int
+}
+
+func NewIterator(data *Vector, pos int) *Iterator {
+	return &Iterator{data: data, pos: pos}
+}
+
+func (iter *Iterator) Next() iterator.RandomAccessIterator {
+	if iter.pos == iter.data.Size() {
+		return iter
+	}
+	iter.pos++
+	return iter
+}
+
+func (iter *Iterator) NextN(n int) iterator.RandomAccessIterator {
+	if iter.pos+n == iter.data.Size() {
+		return iter
+	}
+	iter.pos += n
+	return iter
+}
+
+func (iter *Iterator) Pre() iterator.RandomAccessIterator {
+	if iter.pos == -1 {
+		return iter
+	}
+	iter.pos--
+	return iter
+}
+
+func (iter *Iterator) PreN(n int) iterator.RandomAccessIterator {
+	if iter.pos-n == -1 {
+		return iter
+	}
+	iter.pos -= n
+	return iter
+}
+
+func (iter *Iterator) IteratorAt(pos int) iterator.RandomAccessIterator {
+	if pos < 0 {
+		pos = 0
+	}
+	if pos > iter.data.Size() {
+		pos = iter.data.Size()
+	}
+	iter.pos = pos
+	return iter
+}
+
+func (iter *Iterator) Clone() iterator.RandomAccessIterator {
+	return NewIterator(iter.data, iter.pos)
+}
+
+func (iter *Iterator) Equal(other iterator.RandomAccessIterator) bool {
+	i, ok := other.(*Iterator)
+	if !ok {
+		return false
+	}
+	return i.data == iter.data && i.pos == iter.pos
+}
+
+func (iter *Iterator) IsFront(other iterator.RandomAccessIterator) bool {
+	i, ok := other.(*Iterator)
+	if !ok {
+		return false
+	}
+	return i.data == iter.data && i.pos < iter.pos
+}
+
+func (iter *Iterator) IsBack(other iterator.RandomAccessIterator) bool {
+	i, ok := other.(*Iterator)
+	if !ok {
+		return false
+	}
+	return i.data == iter.data && i.pos > iter.pos
+
+}
+
+func (iter *Iterator) IsFrontEqual(other iterator.RandomAccessIterator) bool {
+	i, ok := other.(*Iterator)
+	if !ok {
+		return false
+	}
+	return i.data == iter.data && i.pos <= iter.pos
+}
+
+func (iter *Iterator) IsBackEqual(other iterator.RandomAccessIterator) bool {
+	i, ok := other.(*Iterator)
+	if !ok {
+		return false
+	}
+	return i.data == iter.data && i.pos >= iter.pos
+}
+
+func (iter *Iterator) HasNext() bool {
+	return iter.pos == iter.data.Size()
+}
+
+func (iter *Iterator) HasPre() bool {
+	return iter.pos == -1
+}
+
+func (iter *Iterator) IsValid() bool {
+	return iter.pos >= -1 && iter.pos <= iter.data.Size()
+}
+
+func (iter *Iterator) Value() interface{} {
+	return iter.data.At(iter.pos)
+}
+
+func (iter *Iterator) ValueAt(n int) interface{} {
+	return iter.data.At(n)
+}
+
+func (iter *Iterator) SetValue(value interface{}) {
+	iter.data.SetAt(iter.pos, value)
+}
+
+func (iter *Iterator) Distance(other iterator.RandomAccessIterator) int {
+	i, ok := other.(*Iterator)
+	if !ok {
+		return -1
+	}
+	// 求绝对值
+	return (i.pos - iter.pos) ^ ((i.pos - iter.pos) >> 31) - ((i.pos - iter.pos) >> 31)
+}
+
+func (iter *Iterator) Position() int {
+	return iter.pos
+}
---
+++ iterator/iterator.go
@@ -0,0 +1,133 @@
+// @program:     tiny-stl
+// @file:        iterator.go
+// @author:      edte
+// @create:      2021-12-30 08:19
+// @description:
+package iterator
+
+// InputIterator 可读向前遍历的迭代器
+// find 和 accumulate
+type InputIterator interface {
+	// 下一个迭代器
+	Next() InputIterator
+	// 克隆迭代器
+	Clone() InputIterator
+
+	// 判断后面是否还有迭代器
+	HasNext() bool
+	// 比较迭代器是否相等
+	Equal(other InputIterator) bool
+	// 判断迭代器是否有效
+	IsValid() bool
+
+	// 解引用，获取迭代器的值
+	Value() interface{}
+}
+
+// OutputIterator 可写向前遍历的迭代器
+type OutputIterator interface {
+	// 下一个迭代器
+	Next() OutputIterator
+	// 克隆迭代器
+	Clone() OutputIterator
+
+	// 判断后面是否还有迭代器
+	HasNext() bool
+	// 比较迭代器是否相等
+	Equal(other OutputIterator) bool
+	// 判断迭代器是否有效
+	IsValid() bool
+
+	// 设置迭代器的值
+	SetValue(value interface{}) interface{}
+}
+
+// ForwardIterator 可读可写向前遍历的迭代器
+type ForwardIterator interface {
+	// 下一个迭代器
+	Next() ForwardIterator
+	// 克隆迭代器
+	Clone() ForwardIterator
+
+	// 判断后面是否还有迭代器
+	HasNext() bool
+	// 比较迭代器是否相等
+	Equal(other ForwardIterator) bool
+	// 判断迭代器是否有效
+	IsValid() bool
+
+	// 解引用，获取迭代器的值
+	Value() interface{}
+	// 设置迭代器的值
+	SetValue(value interface{}) interface{}
+}
+
+// BidirectionalIterator 可读可写双向遍历迭代器
+// list, set, multiset, map, multimap
+type BidirectionalIterator interface {
+	// 下一个迭代器
+	Next() BidirectionalIterator
+	// 上一个迭代器
+	Pre() BidirectionalIterator
+	// 克隆迭代器
+	Clone() BidirectionalIterator
+
+	// 判断后面是否还有迭代器
+	HasNext() bool
+	// 判断后面是否还有迭代器
+	HasPre() bool
+	// 比较迭代器是否相等
+	Equal(other BidirectionalIterator) bool
+	// 判断迭代器是否有效
+	IsValid() bool
+
+	// 解引用，获取迭代器的值
+	Value() interface{}
+	// 设置迭代器的值
+	SetValue(value interface{}) interface{}
+}
+
+// RandomAccessIterator 可读可写随机访问迭代器
+// vector, deque, string, array
+type RandomAccessIterator interface {
+	// 下一个迭代器
+	Next() RandomAccessIterator
+	// 后第 n 个迭代器
+	NextN(n int) RandomAccessIterator
+	// 上一个迭代器
+	Pre() RandomAccessIterator
+	// 前第 n 个迭代器
+	PreN(n int) RandomAccessIterator
+	// 获取 pos 位置迭代器
+	IteratorAt(pos int) RandomAccessIterator
+	// 克隆迭代器
+	Clone() RandomAccessIterator
+
+	// iter1=iter2
+	Equal(other RandomAccessIterator) bool
+	// iter1<iter2
+	IsFront(other RandomAccessIterator) bool
+	// iter1>iter2
+	IsBack(other RandomAccessIterator) bool
+	// iter1<=iter2
+	IsFrontEqual(other RandomAccessIterator) bool
+	// iter1>=iter2
+	IsBackEqual(other RandomAccessIterator) bool
+	// 判断后面是否还有迭代器
+	HasNext() bool
+	// 判断后面是否还有迭代器
+	HasPre() bool
+	// 判断迭代器是否有效
+	IsValid() bool
+
+	// 解引用，获取迭代器的值
+	Value() interface{}
+	// 解引用，获取第 n 个迭代器的值
+	ValueAt(n int) interface{}
+	// 设置迭代器的值
+	SetValue(value interface{})
+	// 获取两迭代器间的距离
+	Distance(other RandomAccessIterator) int
+	// 获取当前迭代器位置
+	Position() int
+}
