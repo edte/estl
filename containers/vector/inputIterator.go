22df5fc60336cb7ba13d306c713f2ff1f20da703
张彩嵘 <940481948@qq.com>
2021-12-30 17:29:38 +0800

    feat(vector): add vector iterator implements

---
+++ containers/vector/inputIterator.go
@@ -0,0 +1,51 @@
+// @program:     tiny-stl
+// @file:        inputIterator.go
+// @author:      edte
+// @create:      2021-12-30 15:43
+// @description:
+package vector
+
+import (
+	"stl/iterator"
+)
+
+type InputIterator struct {
+	data *Vector
+	pos  int
+}
+
+func NewInputIterator(data *Vector, pos int) *InputIterator {
+	return &InputIterator{data: data.Clone(), pos: pos}
+}
+
+func (iter InputIterator) Next() iterator.InputIterator {
+	if iter.pos == iter.data.Size() {
+		return iter
+	}
+	iter.pos++
+	return iter
+}
+
+func (iter InputIterator) Clone() iterator.InputIterator {
+	return NewInputIterator(iter.data, iter.pos)
+}
+
+func (iter InputIterator) HasNext() bool {
+	return iter.pos == iter.data.Size()
+}
+
+func (iter InputIterator) Equal(other iterator.InputIterator) bool {
+	i, ok := other.(*InputIterator)
+	if !ok {
+		return false
+	}
+	return i.data == iter.data && i.pos == iter.pos
+}
+
+func (iter InputIterator) IsValid() bool {
+	return iter.pos >= -1 && iter.pos <= iter.data.Size()
+}
+
+func (iter InputIterator) Value() interface{} {
+	return iter.data.At(iter.pos)
+}
