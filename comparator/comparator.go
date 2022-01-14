23641eb01ede31c330667e84b906dcee5261c7a5
张彩嵘 <940481948@qq.com>
2022-01-15 01:32:49 +0800

    feat(comparator): modify comparator implements

--- comparator/comparator.go
+++ comparator/comparator.go
@@ -5,19 +5,57 @@
 // @description:
 package comparator
 
+type Operator func(a interface{}, b interface{}) bool
+
+type Cmp func(a interface{}, b interface{}) int
+
 type Comparator interface {
 	Operator(a interface{}, b interface{}) bool
+	Cmp(a interface{}, b interface{}) int
 }
+
+func Reserve(c Comparator) Comparator {
+	return New(func(a interface{}, b interface{}) bool {
+		return !c.Operator(a, b)
+	})
+}
+
+type Option func(d *Default)
 
 type Default struct {
-	cmp func(a interface{}, b interface{}) bool
+	op  Operator
+	cmp Cmp
 }
 
-func New(cmp func(a interface{}, b interface{}) bool) *Default {
-	return &Default{cmp: cmp}
+func New(op Operator, opts ...Option) *Default {
+	d := &Default{
+		op: op,
+	}
+
+	for _, opt := range opts {
+		opt(d)
+	}
+
+	return d
+}
+
+func WithOperator(op Operator) Option {
+	return func(d *Default) {
+		d.op = op
+	}
+}
+
+func WithCmp(cmp Cmp) Option {
+	return func(d *Default) {
+		d.cmp = cmp
+	}
 }
 
 func (d *Default) Operator(a interface{}, b interface{}) bool {
+	return d.op(a, b)
+}
+
+func (d *Default) Cmp(a interface{}, b interface{}) int {
 	return d.cmp(a, b)
 }
 
@@ -28,6 +66,7 @@ func NewGreater() *Greater {
 	return &Greater{}
 }
 
+// Operator 默认相等时不交换，这样许多排序默认就是稳定的
 func (l *Greater) Operator(a interface{}, b interface{}) bool {
 	switch a.(type) {
 	case int:
@@ -53,7 +92,205 @@ func (l *Greater) Operator(a interface{}, b interface{}) bool {
 	}
 }
 
+func (l *Greater) Cmp(a interface{}, b interface{}) int {
+	if a == b {
+		return 0
+	}
+
+	switch a.(type) {
+	case int:
+		if a.(int) > b.(int) {
+			return 1
+		} else if a.(int) == b.(int) {
+			return 0
+		} else {
+			return -1
+		}
+	case int64:
+		if a.(int64) > b.(int64) {
+			return 1
+		} else if a.(int64) == b.(int64) {
+			return 0
+		} else {
+			return -1
+		}
+	case int32:
+		if a.(int32) > b.(int32) {
+			return 1
+		} else if a.(int32) == b.(int32) {
+			return 0
+		} else {
+			return -1
+		}
+	case int16:
+		if a.(int16) > b.(int16) {
+			return 1
+		} else if a.(int16) == b.(int16) {
+			return 0
+		} else {
+			return -1
+		}
+	case int8:
+		if a.(int8) > b.(int8) {
+			return 1
+		} else if a.(int8) == b.(int8) {
+			return 0
+		} else {
+			return -1
+		}
+	case float64:
+		if a.(float64) > b.(float64) {
+			return 1
+		} else if a.(float64) == b.(float64) {
+			return 0
+		} else {
+			return -1
+		}
+	case float32:
+		if a.(float32) > b.(float32) {
+			return 1
+		} else if a.(float32) == b.(float32) {
+			return 0
+		} else {
+			return -1
+		}
+	case bool:
+		if a.(bool) == b.(bool) {
+			return 0
+		} else {
+			return -1
+		}
+	case string:
+		if a.(string) > b.(string) {
+			return 1
+		} else if a.(string) == b.(string) {
+			return 0
+		} else {
+			return -1
+		}
+	default:
+		return 0
+	}
+}
+
+type EGreater struct {
+}
+
+func NewEGreater() *EGreater {
+	return &EGreater{}
+}
+
+func (e *EGreater) Cmp(a interface{}, b interface{}) int {
+	if a == b {
+		return 0
+	}
+
+	switch a.(type) {
+	case int:
+		if a.(int) > b.(int) {
+			return 1
+		} else if a.(int) == b.(int) {
+			return 0
+		} else {
+			return -1
+		}
+	case int64:
+		if a.(int64) > b.(int64) {
+			return 1
+		} else if a.(int64) == b.(int64) {
+			return 0
+		} else {
+			return -1
+		}
+	case int32:
+		if a.(int32) > b.(int32) {
+			return 1
+		} else if a.(int32) == b.(int32) {
+			return 0
+		} else {
+			return -1
+		}
+	case int16:
+		if a.(int16) > b.(int16) {
+			return 1
+		} else if a.(int16) == b.(int16) {
+			return 0
+		} else {
+			return -1
+		}
+	case int8:
+		if a.(int8) > b.(int8) {
+			return 1
+		} else if a.(int8) == b.(int8) {
+			return 0
+		} else {
+			return -1
+		}
+	case float64:
+		if a.(float64) > b.(float64) {
+			return 1
+		} else if a.(float64) == b.(float64) {
+			return 0
+		} else {
+			return -1
+		}
+	case float32:
+		if a.(float32) > b.(float32) {
+			return 1
+		} else if a.(float32) == b.(float32) {
+			return 0
+		} else {
+			return -1
+		}
+	case bool:
+		if a.(bool) == b.(bool) {
+			return 0
+		} else {
+			return -1
+		}
+	case string:
+		if a.(string) > b.(string) {
+			return 1
+		} else if a.(string) == b.(string) {
+			return 0
+		} else {
+			return -1
+		}
+	default:
+		return 0
+	}
+}
+
+func (e *EGreater) Operator(a interface{}, b interface{}) bool {
+	switch a.(type) {
+	case int:
+		return a.(int) >= b.(int)
+	case int64:
+		return a.(int64) >= b.(int64)
+	case int32:
+		return a.(int32) >= b.(int32)
+	case int16:
+		return a.(int16) >= b.(int16)
+	case int8:
+		return a.(int8) >= b.(int8)
+	case float64:
+		return a.(float64) >= b.(float64)
+	case float32:
+		return a.(float32) >= b.(float32)
+	case bool:
+		return a.(bool) != b.(bool)
+	case string:
+		return a.(string) != b.(string)
+	default:
+		return false
+	}
+}
+
 type Less struct {
+}
+
+func (g *Less) Cmp(a interface{}, b interface{}) int {
+	panic("implement me")
 }
 
 func NewLess() *Less {
@@ -76,6 +313,42 @@ func (g *Less) Operator(a interface{}, b interface{}) bool {
 		return a.(float64) < b.(float64)
 	case float32:
 		return a.(float32) < b.(float32)
+	case bool:
+		return a.(bool) != b.(bool)
+	case string:
+		return a.(string) != b.(string)
+	default:
+		return false
+	}
+}
+
+type ELess struct {
+}
+
+func (g *ELess) Cmp(a interface{}, b interface{}) int {
+	panic("implement me")
+}
+
+func NewELess() *ELess {
+	return &ELess{}
+}
+
+func (g *ELess) Operator(a interface{}, b interface{}) bool {
+	switch a.(type) {
+	case int:
+		return a.(int) <= b.(int)
+	case int64:
+		return a.(int64) <= b.(int64)
+	case int32:
+		return a.(int32) <= b.(int32)
+	case int16:
+		return a.(int16) <= b.(int16)
+	case int8:
+		return a.(int8) <= b.(int8)
+	case float64:
+		return a.(float64) <= b.(float64)
+	case float32:
+		return a.(float32) <= b.(float32)
 	case bool:
 		return a.(bool) != b.(bool)
 	case string:
---
+++ comparator/comparator_test.go
@@ -0,0 +1,143 @@
+// @program:     tiny-stl
+// @file:        comparator_test.go.go
+// @author:      edte
+// @create:      2022-01-14 01:11
+// @description:
+package comparator
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestDefault_Operator(t *testing.T) {
+	type fields struct {
+		cmp func(a interface{}, b interface{}) bool
+	}
+	type args struct {
+		a interface{}
+		b interface{}
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := &Default{}
+			if got := d.Operator(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("Operator() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGreater_Operator(t *testing.T) {
+	fmt.Println(Reserve(NewGreater()).Operator(1, 2))
+	fmt.Println((NewGreater()).Operator(1, 2))
+}
+
+func TestLess_Operator(t *testing.T) {
+	type args struct {
+		a interface{}
+		b interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := &Less{}
+			if got := g.Operator(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("Operator() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNew(t *testing.T) {
+	type args struct {
+		cmp func(a interface{}, b interface{}) bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Default
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := New(tt.args.cmp); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("New() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewGreater(t *testing.T) {
+	type args struct {
+		equal []bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Greater
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewGreater(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewGreater() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewLess(t *testing.T) {
+	tests := []struct {
+		name string
+		want *Less
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewLess(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewLess() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_equalGreater_Operator(t *testing.T) {
+	type args struct {
+		a interface{}
+		b interface{}
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			e := &EGreater{}
+			if got := e.Operator(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("Operator() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
