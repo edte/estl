c4aefdfc77e7408efe7663065f2bce72253ab13b
张彩嵘 <940481948@qq.com>
2022-01-22 22:10:10 +0800

    feat(find): add some find algorithms

--- algorithm/find.go
+++ algorithm/find.go
@@ -6,6 +6,7 @@
 package algorithm
 
 import (
+	"stl/comparator"
 	"stl/functional"
 	"stl/iterator"
 )
@@ -13,31 +14,96 @@ import (
 // 一些查找算法
 
 // Find value in range
-func Find() {
+// Returns an iterator to the first element in the range [first,last)
+// that compares equal to val. If no such element is found, the
+// function returns last.
+// The function uses operator== to compare the individual elements
+// to val.
+func Find(first, last iterator.InputIterator, val interface{}) (res iterator.InputIterator) {
+	for i := CloneInput(first); !i.Equal(last); i.Next() {
+		if i.Value() == val {
+			return i
+		}
 
+	}
+	return last
 }
 
-// Find element in range
-func FindIf() {
-
+// FindIf find element in range
+func FindIf(first, last iterator.InputIterator, pred functional.Pred) iterator.InputIterator {
+	for i := CloneInput(first); !i.Equal(last); i.Next() {
+		if pred(i.Value()) {
+			return i
+		}
+	}
+	return last
 }
 
-// Find element in range (negative condition)
-func FindIfNot() {
-
+// FindIfNot find element in range (negative condition)
+func FindIfNot(first, last iterator.InputIterator, pred functional.Pred) iterator.InputIterator {
+	return FindIf(first, last, functional.Not1(pred))
 }
 
-// Find last subsequence in range
-func FindEnd() {
+// FindEnd find last subsequence in range
+func FindEnd(first1, last1, first2, last2 iterator.ForwardIterator, pred ...functional.BinaryPred) iterator.ForwardIterator {
+	p := functional.EqualTo
+	if len(pred) != 0 {
+		p = pred[0]
+	}
 
+	if first2.Equal(last2) {
+		return last1
+	}
+	res := CloneBidirectional(last1)
+
+	i := CloneBidirectional(first1)
+	j := CloneBidirectional(first2)
+
+	for !i.Equal(last1) {
+		i1 := CloneBidirectional(i)
+		j1 := CloneBidirectional(j)
+
+		for !i1.Equal(last1) && !j1.Equal(last2) && p(i1.Value(), j1.Value()) {
+			i1.Next()
+			j1.Next()
+
+			if j1.Equal(last2) {
+				res = CloneBidirectional(i)
+				break
+			}
+
+			if i1.Equal(last1) {
+				return res
+			}
+		}
+
+		i.Next()
+	}
+
+	return res
 }
 
-// Find element from set in range
-func FindFirstOf() {
+// FindFirstOf find element from set in range
+func FindFirstOf(first1, last1 iterator.InputIterator, first2, last2 iterator.ForwardIterator, pred ...functional.BinaryPred) iterator.InputIterator {
+	p := functional.EqualTo
+	if len(pred) != 0 {
+		p = pred[0]
+	}
 
+	i := CloneInput(first1)
+
+	for !i.Equal(last1) {
+		for j := CloneForward(first2); !j.Equal(last2); j.Next() {
+			if p(i.Value(), j.Value()) {
+				return i
+			}
+		}
+		i.Next()
+	}
+	return last1
 }
 
-// Find equal adjacent elements in range
+// AdjacentFind find equal adjacent elements in range
 func AdjacentFind() {
 
 }
@@ -70,8 +136,45 @@ func CountIf(first, last iterator.InputIterator, pred functional.Pred) int {
 }
 
 // Mismatch return first position where two ranges differ
-func Mismatch() {
+func Mismatch(first1, last1, first2 iterator.InputIterator, pred functional.BinaryPred) (iterator.InputIterator, iterator.InputIterator) {
 
+	return nil, nil
+}
+
+// LowerBound return iterator to lower bound
+func LowerBound(first, last iterator.ForwardIterator, val int, cmp ...comparator.Comparator) iterator.ForwardIterator {
+
+	return nil
+}
+
+// UpperBound return iterator to upper bound
+func UpperBound(first, last iterator.ForwardIterator, val int, cmp ...comparator.Comparator) iterator.ForwardIterator {
+
+	return nil
+}
+
+// EqualRange get subrange of equal elements
+func EqualRange(first, last iterator.ForwardIterator, val int, cmp ...comparator.Comparator) (iterator.ForwardIterator, iterator.ForwardIterator) {
+
+	return nil, nil
+}
+
+// BinarySearch test if value exists in sorted sequence
+func BinarySearch(first, last iterator.ForwardIterator, val int, cmp ...comparator.Comparator) bool {
+
+	return false
+}
+
+// Search range for subsequence
+func Search(first1, last1, first2, last2 iterator.ForwardIterator, pred functional.BinaryPred) iterator.ForwardIterator {
+
+	return nil
+}
+
+// SearchN search range for elements
+func SearchN(first, last iterator.ForwardIterator, count, val int, pred functional.BinaryPred) iterator.ForwardIterator {
+
+	return nil
 }
 
 // Equal test whether the elements in two ranges are equal
--- algorithm/find_test.go
+++ algorithm/find_test.go
@@ -6,16 +6,58 @@
 package algorithm
 
 import (
-	"fmt"
+	"github.com/stretchr/testify/assert"
+	"stl/functional"
 	"testing"
 
 	"stl/containers/vector"
 )
 
+func TestFind(t *testing.T) {
+	v := vector.New(vector.WithData([]interface{}{10, 20, 30, 40}))
+	res := Find(v.Begin(), v.End(), 30)
+	assert.Equal(t, 30, res.Value())
+}
+
+func TestFindNot(t *testing.T) {
+	v := vector.New(vector.WithData([]interface{}{10, 20, 30, 40}))
+	res := Find(v.Begin(), v.End(), 22)
+	assert.Equal(t, v.End(), res)
+}
+
+func TestFindIf(t *testing.T) {
+	v := vector.New(vector.WithData([]interface{}{10, 20, 3, 30, 40}))
+	res := FindIf(v.Begin(), v.End(), functional.IsOdd)
+	assert.Equal(t, 3, res.Value())
+}
+
+func TestFindIfNot(t *testing.T) {
+	v := vector.New(vector.WithData([]interface{}{10, 20, 3, 30, 40}))
+	res := FindIfNot(v.Begin(), v.End(), functional.IsOdd)
+	assert.Equal(t, 10, res.Value())
+}
+
+func TestFindEnd(t *testing.T) {
+	v1 := vector.New(vector.WithData([]interface{}{1, 2, 3, 4, 5, 1, 2, 3, 4, 5}))
+	v2 := vector.New(vector.WithData([]interface{}{1, 2, 3}))
+
+	res := FindEnd(v1.Begin(), v1.End(), v2.Begin(), v2.End())
+
+	assert.Equal(t, 5, Position(res))
+
+}
+
+func TestFindFirstOf(t *testing.T) {
+	v1 := vector.New(vector.WithData([]interface{}{'a', 'b', 'c', 'A', 'B', 'C'}))
+	v2 := vector.New(vector.WithData([]interface{}{'A', 'B', 'C'}))
+	res := FindFirstOf(v1.Begin(), v2.End(), v2.Begin(), v2.End())
+	assert.Equal(t, 3, Position(res))
+}
+
 func TestCount(t *testing.T) {
 	v := vector.New(vector.WithData([]interface{}{23, 2, 3, 3, 3, 3, 2, 2}))
 	count := Count(v.Begin(), v.End(), 2)
-	fmt.Println(count)
+	assert.Equal(t, 3, count)
 }
 
 func TestCountIf(t *testing.T) {
@@ -24,7 +66,7 @@ func TestCountIf(t *testing.T) {
 		return i.(int)%2 == 0
 	})
 
-	fmt.Println(res)
+	assert.Equal(t, 3, res)
 }
 
 func TestEqual(t *testing.T) {
@@ -32,5 +74,6 @@ func TestEqual(t *testing.T) {
 	v1 := vector.New(vector.WithData([]interface{}{25, 15, 5, -3, -15}))
 
 	equal := Equal(v.Begin(), v.End(), v1.Begin())
-	fmt.Println(equal)
+
+	assert.False(t, equal)
 }
